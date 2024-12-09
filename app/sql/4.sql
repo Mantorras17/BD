6dd95b0fc1e18d37dd1dbfa4b12134eec01782f2
Hugo Melo <156726216+Mantorras17@users.noreply.github.com>
2024-12-09 17:53:44 +0000

    Update 4.sql

--- app/sql/4.sql
+++ app/sql/4.sql
@@ -1,4 +1,4 @@
---4. Qual é o número total de internamentos por hospital? Mostra o nome do hospital e o número de internamentos, ordenado pelo numero de internamentos descendente.
+--4. Qual e o numero total de internamentos por hospital? Mostra o nome do hospital e o numero de internamentos, ordenado pelo numero de internamentos descendente.
 
 SELECT HOSPITAIS.hospital_id as id, HOSPITAIS.nome as hospital, SUM(ESTATISTICAS.internamentos) AS internamentos
 FROM ESTATISTICAS
