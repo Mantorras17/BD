9b8097473d423d5abcde7bb36c68ff31089b8ca8
Hugo Melo <156726216+Mantorras17@users.noreply.github.com>
2024-12-09 17:55:18 +0000

    Update 6.sql

--- app/sql/6.sql
+++ app/sql/6.sql
@@ -1,4 +1,4 @@
---6. Qual é o número de óbitos por doença? Mostra o id da doença, a descrição da doença e o número de óbitos, ordenado pelo id da doença.
+--6. Qual e o numero de obitos por doenca? Mostra o id da doenca, a descricao da doenca e o numero de obitos, ordenado pelo id da doença.
 
 SELECT DOENCAS.doenca_id as id, DOENCAS.descricao as doenca, SUM(ESTATISTICAS.obitos) as obitos
 FROM ESTATISTICAS
