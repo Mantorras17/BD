97072a6633e4e4625fe7d800ddb7109face7fa9a
Hugo Melo <156726216+Mantorras17@users.noreply.github.com>
2024-12-09 17:57:05 +0000

    Update 9.sql

--- app/sql/9.sql
+++ app/sql/9.sql
@@ -1,4 +1,4 @@
---9. Qual a média de dias de internamento, para cada doença? Mostra o id da doença, a descrição da doença, e a média de dias de internamento. Ordena por média descendente.
+--9. Qual a media de dias de internamento, para cada doenca? Mostra o id da doenca, a descricao da doenca, e a media de dias de internamento. Ordena por media descendente.
 
 SELECT DOENCAS.doenca_id as id, DOENCAS.descricao as doenca, SUM(ESTATISTICAS.dias_de_internamento)/SUM(ESTATISTICAS.internamentos) AS media
 FROM ESTATISTICAS
