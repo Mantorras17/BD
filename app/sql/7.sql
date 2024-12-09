99b180d1eb3ef7a575158331b1e7cc930a0476c3
Hugo Melo <156726216+Mantorras17@users.noreply.github.com>
2024-12-09 17:55:58 +0000

    Update 7.sql

--- app/sql/7.sql
+++ app/sql/7.sql
@@ -1,4 +1,4 @@
---7. Qual a percentagem de óbitos por doença? Mostra o id da doença, a descrição da doença e, dos registados (óbitos+internamentos+ambulatório), a percentagem de quantos foram óbitos. Ordena pela percentagem descendente.
+--7. Qual a percentagem de obitos por doenca? Mostra o id da doenca, a descricao da doenca e, dos registados (obitos+internamentos+ambulatorio), a percentagem de quantos foram obitos. Ordena pela percentagem descendente.
 
 SELECT DOENCAS.doenca_id as id, DOENCAS.descricao as doenca, AVG(ESTATISTICAS.obitos*1.0/(ESTATISTICAS.obitos+ESTATISTICAS.ambulatorio+ESTATISTICAS.internamentos))*100 AS percentagem
 FROM ESTATISTICAS
