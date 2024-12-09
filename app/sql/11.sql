167ec805f263a4f4a86529d9e61f2d03815f7f14
Hugo Melo <156726216+Mantorras17@users.noreply.github.com>
2024-12-09 17:51:33 +0000

    Update 11.sql

--- app/sql/11.sql
+++ app/sql/11.sql
@@ -1,4 +1,4 @@
---11. Como variou o número de óbidos ao longo do tempo? Mostra o periodo e o número de óbidos. Ordena pelo crescente.
+--11. Como variou o numero de obidos ao longo do tempo? Mostra o periodo e o numero de obidos. Ordena pelo crescente.
 
 SELECT ESTATISTICAS.periodo, SUM(ESTATISTICAS.obitos) AS obidos
 FROM ESTATISTICAS
